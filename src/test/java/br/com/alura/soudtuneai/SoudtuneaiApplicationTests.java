eb02456d65285a2561516779fffcfb02ac75fc0b
RevRicardo <revricardolacerda@gmail.com>
2025-05-28 15:43:48 -0300

    Inicializa o projeto

---
+++ src/test/java/br/com/alura/soudtuneai/SoudtuneaiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.alura.soudtuneai;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SoudtuneaiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
